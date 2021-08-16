3f27391b95b6f7f506fc53889fc4126bf3dd440f
leeyongjun <leeyongjun0310@naver.com>
2021-08-16 22:02:28 +0900

    회원가입 구현 완료

--- app.py
+++ app.py
@@ -1,22 +1,78 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 from python.main import run
+
 app = Flask(__name__)
+
+# db연동
+app.config['SECRET_KEY'] = 'this is secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# 데이터베이스 테이블
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    nickname = db.Column(db.String(80), nullable=False )
+
+    def __init__(self, username, nickname, password, **kwargs):
+        self.username = username
+        self.set_password(password)
+        self.nickname = nickname
+
+    # 문자열 형태로 반환
+    def __repr__(self):
+        return f"<User('{self.id}', '{self.username}', '{self.nickname}')>"
+    
+    # 암호화
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    # 암호화된 비밀번호 체크
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/start')
 def start():
     return render_template('start.html')
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        name = request.form['username']
+        passwd = request.form['password']
+        try:
+            data = User.query.filter_by(username=name).first()
+            if data is not None:
+                session['login'] = True
+                return redirect(url_for('index'))
+            else:
+                return "로그인 실패"
+        except:
+            return '로그인 실패'
+
+
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        new_user = User(username=request.form['username'], password=request.form['password'],
+                        nickname=request.form['nickname'])
+        db.session.add(new_user)
+        db.session.commit()
+        return render_template('login.html')
     return render_template('signup.html')
 
 
@@ -53,4 +109,4 @@ def exercise_analysis():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
